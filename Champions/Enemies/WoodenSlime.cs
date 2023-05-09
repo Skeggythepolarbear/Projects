cd27ea835917abc5026d7dfe643a417428669bfc
raccless <59120572+Hallo-web@users.noreply.github.com>
2023-05-09 16:57:05 +0200

    Added new Enemie (Wooden Slime including png)

---
+++ Champions/Enemies/WoodenSlime.cs
@@ -0,0 +1,53 @@
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader.Utilities;
+
+namespace Champions.Enemies
+{
+    public class WoodSlime : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Wooden Slime");
+            Main.npcFrameCount[NPC.type] = Main.npcFrameCount[2];
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 150;
+            NPC.height = 100;
+            NPC.damage = 300;
+            NPC.defense = 1;
+            NPC.lifeMax = 100;
+            NPC.value = 50f;
+            NPC.aiStyle = 1;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            AIType = NPCID.GreenSlime;
+            AnimationType = NPCID.GreenSlime;
+        }
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldDaySlime.Chance * 0.2f;
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            NPC.frameCounter++;
+            if (NPC.frameCounter >= 20)
+            {
+                NPC.frameCounter = 0;
+            }
+            NPC.frame.Y = (int)NPC.frameCounter / 10 * frameHeight;
+        }
+
+        public override void OnKill()
+        {
+            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), ItemID.Gel, Main.rand.Next(0, 2));
+            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), ItemID.Wood, Main.rand.Next(5, 10));
+        }
+
+    }
+}
